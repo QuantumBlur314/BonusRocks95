764f31ee773114fa4823dd5d3f5547c9b8868db7
QuantumBlur314 <assblast420@sludge.bog>
2023-02-24 19:56:30 -0700

    Trying to make it grow instead of shrink

--- BonusRocks95/BonusRocks95.cs
+++ BonusRocks95/BonusRocks95.cs
@@ -3,10 +3,14 @@ using JetBrains.Annotations;
 using OWML.Common;
 using OWML.ModHelper;
 using OWML.ModHelper.Events;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms;
 
 namespace BonusRocks95
 {
@@ -16,10 +20,14 @@ namespace BonusRocks95
     {
         public bool bRHolesShrinkStars;
         public bool bRHolesWarpUnmarkedBodies;
-        public static List<OWRigidbody> _growQueue = new(8);//establishes my own _growQueue (with blackjack, and hookers)
+        public static List<OWRigidbody> _bR95growQueue = new(8);//establishes my own _growQueue (with blackjack, and hookers)
         public OWRigidbody _growingBody;
         public float _nextGrowCheckTime;
-        public SectorDetector _detectorInspector;
+        public Key Big;
+        public bool BigBubbon;
+        public Key Small;
+        public bool SmallBubbon;
+
         public static BonusRocks95 Instance;
 
 
@@ -35,34 +43,89 @@ namespace BonusRocks95
 
         private void Start()
         {
-            BonusRocks95.Instance.ModHelper.Console.WriteLine($"{nameof(BonusRocks95)} rocks into battle 95 times.", MessageType.Success);
-
             // Example of accessing game code.
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
             {
                 if (loadScene != OWScene.SolarSystem) return;
                 BonusRocks95.Instance.ModHelper.Console.WriteLine("Another day...", MessageType.Warning);
             };
-            BonusRocks95.Instance.ModHelper.Console.WriteLine($"{nameof(BonusRocks95)} settings begin screaming backwards", MessageType.Info);
+            BonusRocks95.Instance.ModHelper.Console.WriteLine($"{nameof(BonusRocks95)} begins screaming backwards", MessageType.Info);
         }
         public override void Configure(IModConfig config)
         {
             BonusRocks95.Instance.bRHolesShrinkStars = BonusRocks95.Instance.ModHelper.Config.GetSettingsValue<bool>("Shrink Stars");
             ModHelper.Console.WriteLine($"Shrink Stars: {bRHolesShrinkStars}");
+
+            Big = (Key)System.Enum.Parse(typeof(Key), config.GetSettingsValue<string>("Big Your Ball"));
+            ModHelper.Console.WriteLine($"Button of Big a Ball: {Big}");
+            Small = (Key)System.Enum.Parse(typeof(Key), config.GetSettingsValue<string>("Small Your Ball"));
+            ModHelper.Console.WriteLine($"Button of Small a Ball: {Small}");
+
         }
 
-        public static void GetSectorDetectors()
+        private void Update()
         {
-            foreach (var detecteddetectors in Resources.FindObjectsOfTypeAll<SectorDetector>()) BonusRocks95.Instance.ModHelper.Console.WriteLine(detecteddetectors.ToString());
-            
-        }
+            if (!OWInput.IsInputMode(InputMode.Menu))   //Keybinding code lovingly stolen from BlackHolePortalGun by NagelId, who added keybinding to BHPG specifically because I suggested it.
+            {
+                BigBubbon = Keyboard.current[Big].wasPressedThisFrame; ;
+                SmallBubbon = Keyboard.current[Small].wasPressedThisFrame;   //BHPG listened for .wasReleasedThisFrame here; if this doesn't work, just do that
+            }
 
-        public static void GrowSun(SectorDetector __instance)
+            if (BigBubbon)
+            {
+
+                AddSunToCustomGrowQueue(Locator._centerOfTheUniverse._staticReferenceFrame);
+                BonusRocks95.Instance.ModHelper.Console.WriteLine($"GrowQueue updated:");
+                foreach (var tinybodies in BonusRocks95._bR95growQueue) BonusRocks95.Instance.ModHelper.Console.WriteLine(tinybodies.ToString());
+                CustomFixedUpdate();
+            }
+        }
+        private void AddSunToCustomGrowQueue(OWRigidbody bodyToGrow)
         {
-            if (SectorDetector._detectorInspector != null)
-            { SectorDetector._detectorInspector.OnEnterSector += BonusRocks95.GrowSun; };
-        }
+            bodyToGrow.SetLocalScale(Vector3.one * 0.1f);  //idk what this does but WhiteHoleVolume.AddToGrowQueue does it so I will, too
 
+            if (!BonusRocks95._bR95growQueue.Contains(bodyToGrow))
+            {
+                BonusRocks95._bR95growQueue.Add(bodyToGrow);
+            };
+
+
+
+        }
+        private void CustomFixedUpdate()  //stolen from WhiteHoleVolume.FixedUpdate then mangled beyond recognition
+        {
+            if (_growingBody != null)
+            {
+                _growingBody.SetLocalScale(_growingBody.GetLocalScale() * 1.05f);
+                if (_growingBody.GetLocalScale().x >= 1f)
+                {
+                    BR95FinishGrowing(_growingBody);
+                    _growingBody = null;
+                    return;
+                }
+            }
+            else if (BonusRocks95._bR95growQueue.Count > 0)
+            {
+                if (BonusRocks95._bR95growQueue[0] == null)
+                {
+                    BonusRocks95._bR95growQueue.RemoveAt(0);
+
+
+                    return;
+                }
+                if (Time.time > _nextGrowCheckTime)
+                {
+                    _nextGrowCheckTime = Time.time + 1f;
+                    _growingBody = BonusRocks95._bR95growQueue[0];
+                    BonusRocks95._bR95growQueue.RemoveAt(0);
+
+                }
+            }
+        }
+        private void BR95FinishGrowing(OWRigidbody body)
+        {
+            body.SetLocalScale(Vector3.one);
+        }
 
         [HarmonyPatch]
         public class BonusRocks95PatchClass
@@ -89,70 +152,6 @@ namespace BonusRocks95
 
             }
 
-
-            [HarmonyPrefix, HarmonyPatch(typeof(PauseMenuManager), nameof(PauseMenuManager.OnDeactivatePauseMenu))]
-
-            private static bool AddSunToCustomGrowQueue(OWRigidbody __instance)
-            {
-                if (BonusRocks95.Instance.bRHolesShrinkStars || __instance.CompareTag("Probe") || __instance.CompareTag("Player") || __instance.CompareTag("Ship") || __instance.CompareTag("NomaiShuttleBody"))
-                    return true;  //do nothing if Shrink Stars is enabled, nor if the __instance is any of these losers
-
-                bool itsTheSun = __instance == Locator._centerOfTheUniverse._staticReferenceFrame;
-                BonusRocks95.Instance.ModHelper.Console.WriteLine($"Is it the Sun?");
-
-                if (!itsTheSun)
-                    return true; //if OWRigidbody is the sun, continue running. Otherwise, stop (will probably just stop running if a non-sun rigidbody exists anywhere, dammit)
-
-
-                __instance.SetLocalScale(Vector3.one * 0.1f);  //idk what this does but WhiteHoleVolume.AddToGrowQueue does it so I will, too
-
-                if (!BonusRocks95._growQueue.Contains(__instance))  //wait, will this target EVERY instance of OWRigidBody, not just the sun?  I only want the sun in this Q_Q
-                {
-                    BonusRocks95._growQueue.Add(__instance);
-                    BonusRocks95.Instance.ModHelper.Console.WriteLine($"GrowQueue updated:");
-                    foreach (var tinybodies in BonusRocks95._growQueue) BonusRocks95.Instance.ModHelper.Console.WriteLine(tinybodies.ToString());
-                }
-                return true;
-
-
-
-            }
-            private static void CustomFixedUpdate(OWRigidbody _growingBody)  //stolen from WhiteHoleVolume.FixedUpdate then mangled beyond recognition
-            {
-                if (_growingBody != null)
-                {
-                    BonusRocks95.Instance._growingBody.SetLocalScale(BonusRocks95.Instance._growingBody.GetLocalScale() * 1.05f);
-                    if (BonusRocks95.Instance._growingBody.GetLocalScale().x >= 1f)
-                    {
-                        BR95FinishGrowing(BonusRocks95.Instance._growingBody);
-                        BonusRocks95.Instance._growingBody = null;
-                        return;
-                    }
-                }
-                else if (BonusRocks95._growQueue.Count > 0)
-                {
-                    if (BonusRocks95._growQueue[0] == null)
-                    {
-                        BonusRocks95._growQueue.RemoveAt(0);
-
-                        return;
-                    }
-                    if (Time.time > BonusRocks95.Instance._nextGrowCheckTime)
-                    {
-                        BonusRocks95.Instance._nextGrowCheckTime = Time.time + 1f;
-                        BonusRocks95.Instance._growingBody = BonusRocks95._growQueue[0];
-                        BonusRocks95._growQueue.RemoveAt(0);
-
-                    }
-                }
-            }
-
-            private static bool BR95FinishGrowing(OWRigidbody body)
-            {
-                body.SetLocalScale(Vector3.one);
-                return true;
-            }
-
             [HarmonyPostfix, HarmonyPatch(typeof(VanishVolume), nameof(VanishVolume.Shrink))]
             private static void VanishVolume_LogShrink(VanishVolume __instance, OWRigidbody bodyToShrink)
 
@@ -163,3 +162,4 @@ namespace BonusRocks95
             }
         }
     }
+}
